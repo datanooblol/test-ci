a246057e30375c4b97ad60bc4dd14b1788120767
datanooblol <data.noob.lol@gmail.com>
2019-12-16 17:24:25 +0700

    path changed

--- tests/features/test_get_data_as_numpy_array.py
+++ tests/features/test_get_data_as_numpy_array.py
@@ -8,7 +8,8 @@ def test_on_clean_file():
   					           [1148.0, 206186.0]
                        ]
                       )
-  path = "C:/Users/Admin/Desktop/unit-test-for-data-science-master/unit-test-for-data-science-master/data/clean/example_clean_data.txt"
+  # path = "C:/Users/Admin/Desktop/unit-test-for-data-science-master/unit-test-for-data-science-master/data/clean/example_clean_data.txt"
+  path = "data\clean\example_clean_data.txt"
   actual = get_data_as_numpy_array(path, num_columns=2)
   message = "Expected return value: {0}, Actual Return value: {1}".format(expected, actual)
   # Complete your test
